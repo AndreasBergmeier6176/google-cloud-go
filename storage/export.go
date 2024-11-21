927e27342fb867e2f39fa8515f925c7a51cdeb8b
Andreas Bergmeier <andreas.bergmeier@otto.de>
2024-11-21 16:33:58 +0100

    Export all parameters

--- storage/export.go
+++ storage/export.go
@@ -5,3 +5,33 @@ import "cloud.google.com/go/storage/internal/apiv2/storagepb"
 var RegisterStorageServer = storagepb.RegisterStorageServer
 
 type UnimplementedStorageServer = storagepb.UnimplementedStorageServer
+type Storage_BidiWriteObjectServer = storagepb.Storage_BidiWriteObjectServer
+type Storage_ReadObjectServer = storagepb.Storage_ReadObjectServer
+type StartResumableWriteRequest = storagepb.StartResumableWriteRequest
+type Storage_WriteObjectServer = storagepb.Storage_WriteObjectServer
+
+type Bucket = storagepb.Bucket
+type CancelResumableWriteRequest = storagepb.CancelResumableWriteRequest
+type CancelResumableWriteResponse = storagepb.CancelResumableWriteResponse
+type ComposeObjectRequest = storagepb.ComposeObjectRequest
+type CreateBucketRequest = storagepb.CreateBucketRequest
+type DeleteBucketRequest = storagepb.DeleteBucketRequest
+type DeleteObjectRequest = storagepb.DeleteObjectRequest
+type GetBucketRequest = storagepb.GetBucketRequest
+type GetObjectRequest = storagepb.GetObjectRequest
+type ListBucketsRequest = storagepb.ListBucketsRequest
+type ListBucketsResponse = storagepb.ListBucketsResponse
+type ListObjectsRequest = storagepb.ListObjectsRequest
+type ListObjectsResponse = storagepb.ListObjectsResponse
+type LockBucketRetentionPolicyRequest = storagepb.LockBucketRetentionPolicyRequest
+type Object = storagepb.Object
+type QueryWriteStatusRequest = storagepb.QueryWriteStatusRequest
+type QueryWriteStatusResponse = storagepb.QueryWriteStatusResponse
+type RestoreObjectRequest = storagepb.RestoreObjectRequest
+type ReadObjectRequest = storagepb.ReadObjectRequest
+type ReadObjectResponse = storagepb.ReadObjectResponse
+type RewriteObjectRequest = storagepb.RewriteObjectRequest
+type RewriteResponse = storagepb.RewriteResponse
+type StartResumableWriteResponse = storagepb.StartResumableWriteResponse
+type UpdateBucketRequest = storagepb.UpdateBucketRequest
+type UpdateObjectRequest = storagepb.UpdateObjectRequest
